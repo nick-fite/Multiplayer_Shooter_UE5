2d123cce24b79cef44540d5d6573b85dc1efa1b7
Fite <nfite@student.uiwtx.edu>
2024-04-11 18:06:03 -0500

    added player movement

--- Source/GP2_Final/Private/PlayerCharacter.cpp
+++ Source/GP2_Final/Private/PlayerCharacter.cpp
@@ -2,20 +2,45 @@
 
 
 #include "PlayerCharacter.h"
+
+#include "GameFramework/SpringArmComponent.h"
+#include "camera/CameraComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/CharacterMovementComponent.h"
 // Sets default values
 APlayerCharacter::APlayerCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>("Camera Boom");
+	ViewCamera = CreateDefaultSubobject<UCameraComponent>("View Camera");
+
+	CameraBoom->SetupAttachment(GetRootComponent());
+	ViewCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+
+	CameraBoom->bUsePawnControlRotation = true;
+	CameraBoom->TargetArmLength = 800.f;
+	bUseControllerRotationYaw = false;
 	
-	
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(1080.f);
+	GetCharacterMovement()->JumpZVelocity = 400.f;
+
 }
 
 // Called when the game starts or when spawned
 void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	if(APlayerController* playerController = Cast<APlayerController>(GetController()))
+	{
+		if(UEnhancedInputLocalPlayerSubsystem* subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(playerController->GetLocalPlayer()))
+		{
+			subsystem->AddMappingContext(MappingContext,0);
+		}
+	}
 }
 
 // Called every frame
@@ -29,5 +54,42 @@ void APlayerCharacter::Tick(float DeltaTime)
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if(UEnhancedInputComponent* enhancedInputComp = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		enhancedInputComp->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Move);
+		enhancedInputComp->BindAction(LookAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Look);
+		enhancedInputComp->BindAction(JumpAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Jump);
+		
+	}
+}
+
+void APlayerCharacter::Move(const FInputActionValue& InputValue)
+{
+	FVector2D input = InputValue.Get<FVector2D>();
+	input.Normalize();
+	AddMovementInput(input.Y * GetMoveForwardDir() + input.X * GetMoveRightDir());
+
+}
+
+void APlayerCharacter::Look(const FInputActionValue& InputValue)
+{
+	const FVector2D input = InputValue.Get<FVector2D>();
+	AddControllerYawInput(input.X * LookSensitivity);
+	AddControllerPitchInput(-input.Y * LookSensitivity);
+
+}
+
+FVector APlayerCharacter::GetMoveForwardDir() const
+{
+	FVector cameraForward = ViewCamera->GetForwardVector();
+	cameraForward.Z = 0;
+	return cameraForward.GetSafeNormal();
+
+}
+
+FVector APlayerCharacter::GetMoveRightDir() const
+{
+	return ViewCamera->GetRightVector();
 }
 
--- Source/GP2_Final/Public/PlayerCharacter.h
+++ Source/GP2_Final/Public/PlayerCharacter.h
@@ -6,6 +6,12 @@
 #include "GameFramework/Character.h"
 #include "PlayerCharacter.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
+struct FInputActionValue;
+class UInputMappingContext;
+class UInputAction;
+
 UCLASS()
 class GP2_FINAL_API APlayerCharacter : public ACharacter
 {
@@ -27,5 +33,21 @@ public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 private:
-		UFUNCTION() void Move();
+	UFUNCTION() void Move(const FInputActionValue& InputValue);
+	UFUNCTION() void Look(const FInputActionValue& InputValue);
+
+	UFUNCTION() FVector GetMoveForwardDir() const;
+	UFUNCTION() FVector GetMoveRightDir() const;
+	
+	//camera
+	UPROPERTY(VisibleAnywhere, Category="View") USpringArmComponent* CameraBoom;
+	UPROPERTY(VisibleAnywhere, Category="View") UCameraComponent* ViewCamera;
+
+
+	UPROPERTY(EditAnywhere, Category = "Input") UInputMappingContext* MappingContext;
+	UPROPERTY(EditAnywhere, category="Input") UInputAction* MoveAction;
+	UPROPERTY(EditAnywhere, category="Input") UInputAction* JumpAction;
+	UPROPERTY(EditAnywhere, category="Input") UInputAction* LookAction;
+	UPROPERTY(EditAnywhere, Category = "Input") float LookSensitivity {0.5f};
+	
 };
